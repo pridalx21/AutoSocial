b1fb2e90f43aa5031cb915b228929fab97d79fbb
pridalx21 <123890682+pridalx21@users.noreply.github.com>
2024-12-31 17:58:40 +0100

    Prepare application for Render.com deployment

--- robethood_api.py
+++ robethood_api.py
@@ -1,5 +1,11 @@
 import os
 import requests
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
 
 class RobethoodAPI:
     def __init__(self):
@@ -12,7 +18,17 @@ class RobethoodAPI:
         response = requests.get(url, headers=headers)
         return response.json()
 
-# Beispielverwendung:
-# api = RobethoodAPI()
-# data = api.get_data('dein_endpoint')
-# print(data)
+api = RobethoodAPI()
+
+@app.route('/')
+def home():
+    return jsonify({"status": "ok", "message": "Robethood API is running"})
+
+@app.route('/api/<endpoint>')
+def get_data(endpoint):
+    data = api.get_data(endpoint)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
